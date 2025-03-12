c9aba2d23f04db82d6abff876f798fcc8e69f229
kiennt <kiennt@vuihoc.vn>
2025-03-12 11:37:58 +0700

    use webview

--- lib/presentation/webview_screen.dart
+++ lib/presentation/webview_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 class WebViewScreen extends StatefulWidget {
   const WebViewScreen({Key? key}) : super(key: key);
@@ -21,10 +18,6 @@ class _WebViewScreenState extends State<WebViewScreen> {
   }
 
   InAppWebViewController? controller;
-  Future<void> request() async {
-    await Permission.camera.request();
-    await Permission.microphone.request();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,7 +41,7 @@ class _WebViewScreenState extends State<WebViewScreen> {
           this.controller = controller;
           this
               .controller
-              ?.loadUrl(urlRequest: URLRequest(url: Uri.parse(text)));
+              ?.loadUrl(urlRequest: URLRequest(url: Uri.parse(text.trim())));
         },
       ),
     );
